ff486f9a7c00867d3de4c5d461e78972fb2ac38b
Yogya Singhal <yogyasinghal21@gmail.com>
2026-06-16 16:24:04 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery offers a wide variety of beautiful and healthy houseplants
+        to brighten homes and workplaces.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
